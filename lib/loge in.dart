e50f3f8625d383742840ea88b633caefc1bce44d
Momen Hassan Salman <momensalman.dev@gmail.com>
2026-02-08 12:09:02 +0200

    ss
    dd

---
+++ lib/loge in.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:momensalman/ToDo.dart';
+import 'package:momensalman/insta.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+void   dispose () {
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ToDo (),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        elevation: 100,
+        backgroundColor: Colors.blueGrey,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+
+
+
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+        ],
+
+      ),
+      body: const Center(
+        child: Text("Hello Flutter"),
+      ),
+    );
+  }
+}
